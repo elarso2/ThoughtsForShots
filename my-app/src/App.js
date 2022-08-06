66880988ff326b4412887bb86399819365200ec9
Elizabeth Larson <elarso2@uw.edu>
2022-08-06 11:44:11 -0700

    creating comment seeds, removing route folder, adding alt logo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,27 +4,27 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Forum from './pages/Forum';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import {Header} from './components/Header';
-import {ProfileCard} from './components/ProfileCard';
-import {ThoughtsPage} from './components/ThoughtsPage';
+import { Header } from './components/Header';
+import { ProfileCard } from './components/ProfileCard';
+import { ThoughtsPage } from './components/ThoughtsPage';
 
 function App() {
   return (
-    
-      <ChakraProvider>
-      <ColorModeSwitcher 
-      justifySelf="flex-end"
-       position="absolute" 
-       top={4} 
-       right={3} />
+    <ChakraProvider>
+      <ColorModeSwitcher
+        justifySelf="flex-end"
+        position="absolute"
+        top={4}
+        right={3}
+      />
       {/* <Login /> */}
-        <Signup />
+      <Signup />
       {/* <Forum /> */}
       <Header />
+      <Routes></Routes>
       <ProfileCard />
       <ThoughtsPage />
-      </ChakraProvider>
-    
+    </ChakraProvider>
   );
 }
 

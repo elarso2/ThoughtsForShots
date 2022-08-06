9aff1aacabba79ee0f61ba32bde033b4b99c33c7
Xavier_Marquez <xavier.ray.marquez@gmail.com>
2022-08-06 13:46:56 -0700

    adding stripe reslovers and quries

--- my-app/src/utils/queries.js
+++ my-app/src/utils/queries.js
@@ -36,33 +36,3 @@ export const QUERY_ALL_PRODUCTS = gql`
     }
   }
 `;
-
-export const QUERY_CATEGORIES = gql`
-  {
-    categories {
-      _id
-      name
-    }
-  }
-`;
-
-export const QUERY_USER = gql`
-  {
-    user {
-      firstName
-      lastName
-      orders {
-        _id
-        purchaseDate
-        products {
-          _id
-          name
-          description
-          price
-          quantity
-          image
-        }
-      }
-    }
-  }
-`;

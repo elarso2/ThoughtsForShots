3b5dc9dd04b7a67bc2b719d6913133d5ff7a5d35
AR31313 <gurlgotgame@gmail.com>
2022-08-08 19:21:59 -0700

    mutations:createthought,addcomment & remove

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -47,7 +47,14 @@ const typeDefs = gql`
     addUser(username: String!, email: String!, thoughts: [Thought]): Auth
     updateUser(username: String, email: String): User
     login(email: String!): Auth
-    addComment
+    createThought(thoughtText: String!, thoughtAuthor: String!): Thought
+    addComment(
+      thoughtId: ID!
+      commentText: String!
+      author: String!
+    ): Thought
+    removeThought(thoughtId: ID!): Thought
+    removeComment(thoughtId: ID!, commentId: ID!): Thought
   }
 `;
 

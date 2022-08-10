f37aa7e0ad6c7c3d79da236fa2d65fcf588a4f82
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-08-09 21:45:04 -0700

    some changes to pages

--- my-app/src/pages/Forum.jsx
+++ my-app/src/pages/Forum.jsx
@@ -3,12 +3,13 @@ import { Header } from '../components/Header';
 import { ProfileCard } from '../components/ProfileCard';
 import { ThoughtsPage } from '../components/ThoughtsPage';
 import { Footer } from '../components/Footer';
-import Cart from '../components/Cart';
+// import Cart from '../components/Cart';
+
 const Home = () => {
   return (
     <div>
-      <Header />
-      <Cart />
+      <Header /> 
+      {/* <Cart /> */}
       <ProfileCard />
       <ThoughtsPage />
       <Footer />

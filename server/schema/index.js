a9dbd4957cdcab73e59fbe294c9ca924a48d3761
Elizabeth Larson <elarso2@uw.edu>
2022-08-09 23:58:40 -0700

    commenting out some lines, fixing cart link

--- server/schema/index.js
+++ server/schema/index.js
@@ -1,5 +1,5 @@
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
-console.log(typeDefs);
-console.log(resolvers);
+// console.log(typeDefs);
+// console.log(resolvers);
 module.exports = { typeDefs, resolvers };

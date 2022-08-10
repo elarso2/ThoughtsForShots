3f66752bc5ae8add316298e05573828f9083dc75
AR31313 <gurlgotgame@gmail.com>
2022-08-10 11:19:17 -0700

    chnaging dbname back to original

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/hello",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/thoughts-for-shots",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,

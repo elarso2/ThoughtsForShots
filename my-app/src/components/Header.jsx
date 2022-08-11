3cc312b11e484fc1c3335472455857c917177a4c
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-08-10 18:11:31 -0700

    added name on login page

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -47,8 +47,6 @@ export function Header() {
         <Button w="120px" h="40px" bg="yellow.700">
           Order History
         </Button>
-
-
         <Button w="110px" h="40px" bg="yellow.600">
           Pay Your Tab
         </Button>

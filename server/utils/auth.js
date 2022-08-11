e16a57cf6c8098ea8abaf5346bd97971c7d744c9
Rafael Herrera <rafaelherrera731@gmail.com>
2022-08-10 21:08:54 -0700

    token issue resolved

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -28,4 +28,8 @@ module.exports = {
     // next();
     return req;
   },
+  signToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
 };

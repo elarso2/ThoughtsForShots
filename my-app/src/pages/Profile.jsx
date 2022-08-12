f34a640a70ec5656e677cfc0805a7288a9ccde97
Elizabeth Larson <elarso2@uw.edu>
2022-08-11 22:58:01 -0700

    add card to profile, update readme

--- my-app/src/pages/Profile.jsx
+++ my-app/src/pages/Profile.jsx
@@ -1,11 +1,15 @@
-import React from "react";
-import NavBar from "../components/NavBar";
-import { Header } from "../components/Header";
+import React from 'react';
+import NavBar from '../components/NavBar';
+import { Header } from '../components/Header';
+import { ProfileCard } from '../components/ProfileCard';
 
 const Profile = () => {
-    return (
-        <Header />
-    )
+  return (
+    <div>
+      <Header />
+      <ProfileCard />
+    </div>
+  );
 };
 
 export default Profile;

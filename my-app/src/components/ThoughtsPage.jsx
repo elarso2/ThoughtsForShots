f669704c24ea4e53c288a50e2d42cb43c9f5d8c3
Xavier_Marquez <xavier.ray.marquez@gmail.com>
2022-08-12 22:10:12 -0700

    trying to fix submit graphql checkout

--- my-app/src/components/ThoughtsPage.jsx
+++ my-app/src/components/ThoughtsPage.jsx
@@ -11,8 +11,6 @@ import { useStoreContext } from '../utils/GlobalState';
 import { ADD_TO_CART } from '../utils/actions';
 import ProfilePic from '../assets/t-pain.jpg';
 
-
-
 export function ThoughtsPage({ thoughts, showUsername = true }) {
   const [state, dispatch] = useStoreContext();
 
@@ -36,38 +34,39 @@ export function ThoughtsPage({ thoughts, showUsername = true }) {
       textAlign="center"
       as="section"
     >
+      <Flex>
+        <Box bg="#F0EAFB" p="60px">
+          <Image
+            borderRadius="full"
+            boxSize="200px"
+            src={ProfilePic}
+            alt="t-pain"
+            ml="45px"
+          />
+          <Button
+            onClick={addToTab}
+            colorScheme="purple"
+            size="lg"
+            w="282px"
+            mt="24px"
+          >
+            Buy a shot $4
+          </Button>
+        </Box>
+        <Box p="60px" fontSize="18px" bg="white">
+          <Stack as="ul" spacing="20px" pt="24px">
+            <Container mt="36px">
+              It's always T-Pain "Buy me a Drank" but never "T-Pain, let me buy
+              YOU a drank" :/{' '}
+            </Container>
 
-        <Flex>
-                <Box bg="#F0EAFB" p="60px">
-                  
-                  <Image
-                    borderRadius="full"
-                    boxSize="200px"
-                    src={ProfilePic}
-                    alt="t-pain"
-                    ml="45px"
-                  />
-                  <Button
-                    onClick={addToTab}
-                    colorScheme="purple"
-                    size="lg"
-                    w="282px"
-                    mt="24px"
-                  >
-                    Buy a shot $4
-                  </Button>
-                </Box>
-                <Box p="60px" fontSize="18px" bg="white">
-                  <Stack as="ul" spacing="20px" pt="24px">
-                    <Container mt="36px">It's allways T-Pain "Buy me a Drank" but never "T-Pain, let me buy YOU a drank" :/ </Container>
-                    
-                    <Textarea minW="500px" placeholder="Comment" />
-                    <Button colorScheme="purple" size="lg" w="282px">
-                      Reply
-                    </Button>
-                  </Stack>
-                </Box>
-        </Flex>
+            <Textarea minW="500px" placeholder="Comment" />
+            <Button colorScheme="purple" size="lg" w="282px">
+              Reply
+            </Button>
+          </Stack>
+        </Box>
+      </Flex>
     </Box>
   );
 }
